6a4d30540c9c08550ee3925329cd7ef3ecadddd9
obipranata <obiimanuelpranata@gmail.com>
2023-01-10 20:13:05 +0700

    (+) fix bug

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -1,150 +1,152 @@
 document.addEventListener('alpine:init', () => {
-    Alpine.data('user', () => ({
-        isLogedIn: false,
-        baseUrl: "http://127.0.0.1:8000/",
-        apiUrl: "http://127.0.0.1:8001/api/",
-        imgUrl: "http://127.0.0.1:8001/",
-        subscribe_status: false,
-        linkInputFacebook: true,
-        linkInputLinkedin: false,
-        linkInputInstagram: false,
-        linkInputTwitter: false,
-        data_user: [],
-        showFlash: false,
+  Alpine.data('user', () => ({
+    isLogedIn: false,
+    baseUrl: "http://127.0.0.1:8000/",
+    apiUrl: "http://127.0.0.1:8001/api/",
+    imgUrl: "http://127.0.0.1:8001/",
+    subscribe_status: false,
+    linkInputFacebook: true,
+    linkInputLinkedin: false,
+    linkInputInstagram: false,
+    linkInputTwitter: false,
+    data_user: [],
+    showFlash: false,
+    isLoading: false,
+    listMyArticle: [],
 
-        flash() {
-            if (localStorage.getItem('showFlash')) {
-                this.showFlash = true;
-                setTimeout(function () {
-                    localStorage.removeItem("showFlash")
-                    this.showFlash = false;
-                }, 3000);
-            }
+    flash() {
+      if (localStorage.getItem('showFlash')) {
+        this.showFlash = true;
+        setTimeout(function () {
+          localStorage.removeItem("showFlash")
+          this.showFlash = false;
+        }, 3000);
+      }
+    },
+    name: '',
+    username: '',
+    email: '',
+    password: '',
+    photo: '',
+    link_facebook: '',
+    link_linkedin: '',
+    link_instagram: '',
+    link_twitter: '',
+
+    checkSession() {
+      const token = localStorage.getItem('token')
+      this.isLogedIn = token ? true : false
+
+      if (!this.isLogedIn) {
+        // Fetch API Check Token
+        return window.location.replace(this.baseUrl + 'login')
+      }
+    },
+
+    fetchMe() {
+      fetch(this.apiUrl + 'me', {
+        method: "GET",
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': localStorage.getItem('token')
         },
-        name: '',
-        username: '',
-        email: '',
-        password: '',
-        photo: '',
-        link_facebook: '',
-        link_linkedin: '',
-        link_instagram: '',
-        link_twitter: '',
+      })
+        .then(async response => {
+          user = await response.json();
+          this.data_user = user.data
+          this.name = user.data.name != null ? user.data.name : ''
+          this.username = user.data.username != null ? user.data.username : ''
+          this.email = user.data.email != null ? user.data.email : ''
+          this.password = user.data.password != null ? user.data.password : ''
+          this.photo = user.data.photo != null ? user.data.photo : ''
+          this.link_facebook = user.data.link_facebook != null ? user.data.link_facebook : ''
+          this.link_linkedin = user.data.link_linkedin != null ? user.data.link_linkedin : ''
+          this.link_instagram = user.data.link_instagram != null ? user.data.link_instagram : ''
+          this.link_twitter = user.data.link_twitter != null ? user.data.link_twitter : ''
+        });
+    },
 
-        checkSession() {
-            const token = localStorage.getItem('token')
-            this.isLogedIn = token ? true : false
+    updateMe() {
+      // console.log(document.getElementById('photo').files[0]);
 
-            if (!this.isLogedIn) {
-                // Fetch API Check Token
-                return window.location.replace(this.baseUrl + 'login')
-            }
+      let photoProfile = document.getElementById('photo').files[0];
+
+      if (photoProfile == undefined) {
+        photoProfile = this.data_user.photo;
+      }
+
+      let formData = new FormData();
+      formData.append('name', this.name ? this.name : '');
+      formData.append('username', this.username ? this.username : '');
+      formData.append('email', this.email ? this.email : '');
+      if (this.password.length != 0) {
+        formData.append('password', this.password);
+      }
+      formData.append('photo', photoProfile);
+      formData.append('link_facebook', this.link_facebook ? this.link_facebook : '');
+      formData.append('link_linkedin', this.link_linkedin ? this.link_linkedin : '');
+      formData.append('link_instagram', this.link_instagram ? this.link_instagram : '');
+      formData.append('link_twitter', this.link_twitter ? this.link_twitter : '');
+
+      fetch(this.apiUrl + 'profileUpdate', {
+        method: "POST",
+        headers: {
+          'Authorization': localStorage.getItem('token')
         },
+        body: formData
+      })
+        .then(async response => {
+          user = await response.json();
+          this.data_user = user.data != null ? user.data : ''
+          this.name = user.data.name != null ? user.data.name : ''
+          this.username = user.data.username != null ? user.data.username : ''
+          this.email = user.data.email != null ? user.data.email : ''
+          this.password = user.data.password != null ? user.data.password : ''
+          this.photo = user.data.photo != null ? user.data.photo : ''
+          this.link_facebook = user.data.link_facebook != null ? user.data.link_facebook : ''
+          this.link_linkedin = user.data.link_linkedin != null ? user.data.link_linkedin : ''
+          this.link_instagram = user.data.link_instagram != null ? user.data.link_instagram : ''
+          this.link_twitter = user.data.link_twitter != null ? user.data.link_twitter : ''
+        })
+    },
 
-        fetchMe() {
-            fetch(this.apiUrl + 'me', {
-                    method: "GET",
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': localStorage.getItem('token')
-                    },
-                })
-                .then(async response => {
-                    user = await response.json();
-                    this.data_user = user.data
-                    this.name = user.data.name != null ? user.data.name : ''
-                    this.username = user.data.username != null ? user.data.username : ''
-                    this.email = user.data.email != null ? user.data.email : ''
-                    this.password = user.data.password != null ? user.data.password : ''
-                    this.photo = user.data.photo != null ? user.data.photo : ''
-                    this.link_facebook = user.data.link_facebook != null ? user.data.link_facebook : ''
-                    this.link_linkedin = user.data.link_linkedin != null ? user.data.link_linkedin : ''
-                    this.link_instagram = user.data.link_instagram != null ? user.data.link_instagram : ''
-                    this.link_twitter = user.data.link_twitter != null ? user.data.link_twitter : ''
-                });
-        },
+    logout() {
+      localStorage.clear()
+      window.location.replace(this.baseUrl + 'login')
+    },
 
-        updateMe() {
-            // console.log(document.getElementById('photo').files[0]);
+    fetchListMyArticle() {
+      const token = localStorage.getItem('token')
 
-            let photoProfile = document.getElementById('photo').files[0];
+      this.isLoading = true,
+        fetch(`${this.apiUrl}myArticle`, {
+          method: "GET",
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.getItem('token')
+          }
+        })
+          .then(async (response) => {
+            this.listMyArticle = await response.json()
+            console.log(this.listMyArticle)
+            this.isLoading = false
 
-            if (photoProfile == undefined) {
-                photoProfile = this.data_user.photo;
-            }
+          })
 
-            let formData = new FormData();
-            formData.append('name', this.name ? this.name : '');
-            formData.append('username', this.username ? this.username : '');
-            formData.append('email', this.email ? this.email : '');
-            if (this.password.length != 0) {
-                formData.append('password', this.password);
-            }
-            formData.append('photo', photoProfile);
-            formData.append('link_facebook', this.link_facebook ? this.link_facebook : '');
-            formData.append('link_linkedin', this.link_linkedin ? this.link_linkedin : '');
-            formData.append('link_instagram', this.link_instagram ? this.link_instagram : '');
-            formData.append('link_twitter', this.link_twitter ? this.link_twitter : '');
+    },
 
-            fetch(this.apiUrl + 'profileUpdate', {
-                    method: "POST",
-                    headers: {
-                        'Authorization': localStorage.getItem('token')
-                    },
-                    body: formData
-                })
-                .then(async response => {
-                    user = await response.json();
-                    this.data_user = user.data != null ? user.data : ''
-                    this.name = user.data.name != null ? user.data.name : ''
-                    this.username = user.data.username != null ? user.data.username : ''
-                    this.email = user.data.email != null ? user.data.email : ''
-                    this.password = user.data.password != null ? user.data.password : ''
-                    this.photo = user.data.photo != null ? user.data.photo : ''
-                    this.link_facebook = user.data.link_facebook != null ? user.data.link_facebook : ''
-                    this.link_linkedin = user.data.link_linkedin != null ? user.data.link_linkedin : ''
-                    this.link_instagram = user.data.link_instagram != null ? user.data.link_instagram : ''
-                    this.link_twitter = user.data.link_twitter != null ? user.data.link_twitter : ''
-                })
-        },
-
-        logout() {
-            localStorage.clear()
-            window.location.replace(this.baseUrl + 'login')
-        },
-
-        fetchListMyArticle() {
-            const token = localStorage.getItem('token')
-
-            this.isLoading = true,
-                fetch(`${this.apiUrl}myArticle`, {
-                    method: "GET",
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': localStorage.getItem('token')
-                    }
-                })
-                .then(async (response) => {
-                    this.listMyArticle = await response.json()
-                    console.log(this.listMyArticle)
-                    this.isLoading = false
-
-                })
-
-        },
-
-        deleteArticle(id) {
-            const token = localStorage.getItem('token')
-            fetch(this.apiUrl + 'article/' + id + '/delete', {
-                    method: "POST",
-                    headers: {
-                        'Authorization': token
-                    }
-                })
-                .then((response) => {
-                    console.log(response)
-                    window.location.replace(this.baseUrl + 'myarticle')
-                });
+    deleteArticle(id) {
+      const token = localStorage.getItem('token')
+      fetch(this.apiUrl + 'article/' + id + '/delete', {
+        method: "POST",
+        headers: {
+          'Authorization': token
         }
-    }))
+      })
+        .then((response) => {
+          console.log(response)
+          window.location.replace(this.baseUrl + 'myarticle')
+        });
+    }
+  }))
 })
