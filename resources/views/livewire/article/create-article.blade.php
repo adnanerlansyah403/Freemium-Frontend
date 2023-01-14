9559fa2a41651021cdfadfbc10064a76fd94e011
obipranata <obiimanuelpranata@gmail.com>
2023-01-15 02:29:24 +0700

    (+) validate 3 free sub articles

--- resources/views/livewire/article/create-article.blade.php
+++ resources/views/livewire/article/create-article.blade.php
@@ -149,6 +149,13 @@
                     </button>
                 </div>
 
+                <template x-if="status_err.min_free">
+                    <div class="mt-3 flex text-[#b91c1c] items-center gap-2">
+                        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+                        <span class="span-danger" x-text="status_err.min_free[0]">Validasi Error</span>
+                    </div>
+                </template>
+
                 <div class="w-full my-1">
                     <ul class="flex flex-col" id="listsubarticle" x-ref="listsubarticle">
                         
