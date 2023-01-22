d6bfcfe51aa57ba73b37f7845cd65a0dc9547621
Adnan Erlansyah <adnanerlansyah403@gmail.com>
2023-01-22 14:52:56 +0700

    updated zindex for loadingpage

--- resources/views/components/loading-page.blade.php
+++ resources/views/components/loading-page.blade.php
@@ -1,4 +1,4 @@
-<div class="fixed w-full h-full top-0 left-0 right-0 bottom-0 bg-white dark:bg-slate-secondary grid place-items-center z-[100]">
+<div class="fixed w-full h-full top-0 left-0 right-0 bottom-0 bg-white dark:bg-slate-secondary grid place-items-center z-[102]">
     <div class="group relative lg:left-0 max-w-[125px]">
         <div class="relative flex flex-wrap z-50 circle hover:rotate-[360deg] transition duration-1000 ease-in-out">
             <span class="block open-up-left-box w-[60px] h-[60px] bg-primary dark:bg-white transition duration-200 ease-in-out"></span>
