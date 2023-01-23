8c47f7baa2e2348c6ed7623ddcb6fec3e2698062
diahnkha <diahnurkhasanah5@gmail.com>
2023-01-23 13:32:44 +0700

    (+) add underline dark mode hover on header

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -10,27 +10,27 @@
 
         @if($currentRoute == 'homepage')
             <ul class="hidden lg:flex items-center gap-10">
-                <li class="relative text-base font-medium before:absolute before:w-full before:h-[1px] before:bottom-0 before:bg-primary before:-left-full hover:before:left-0 before:transiton before:duration-200 before:ease-in-out overflow-x-hidden">
+                <li class="relative text-base font-medium before:absolute before:w-full before:h-[1px] before:bottom-0 before:bg-primary dark:before:bg-slate-third before:-left-full hover:before:left-0 before:transiton before:duration-200 before:ease-in-out overflow-x-hidden">
                     <a href="#header">
                         <span class="span dark:text-slate-third">H</span>ome
                     </a>
                 </li>
-                <li class="relative text-base font-medium before:absolute before:w-full before:h-[1px] before:bottom-0 before:bg-primary before:-left-full hover:before:left-0 before:transiton before:duration-200 before:ease-in-out overflow-x-hidden">
+                <li class="relative text-base font-medium before:absolute before:w-full before:h-[1px] before:bottom-0 before:bg-primary dark:before:bg-slate-third before:-left-full hover:before:left-0 before:transiton before:duration-200 before:ease-in-out overflow-x-hidden">
                     <a href="{{ route('article.list') }}">
                         <span class="span dark:text-slate-third">A</span>rticle
                     </a>
                 </li>
-                <li class="relative text-base font-medium before:absolute before:w-full before:h-[1px] before:bottom-0 before:bg-primary before:-left-full hover:before:left-0 before:transiton before:duration-200 before:ease-in-out overflow-x-hidden">
+                <li class="relative text-base font-medium before:absolute before:w-full before:h-[1px] before:bottom-0 before:bg-primary dark:before:bg-slate-third before:-left-full hover:before:left-0 before:transiton before:duration-200 before:ease-in-out overflow-x-hidden">
                     <a href="#about">
                         <span class="span dark:text-slate-third">A</span>bout
                     </a>
                 </li>
-                <li class="relative text-base font-medium before:absolute before:w-full before:h-[1px] before:bottom-0 before:bg-primary before:-left-full hover:before:left-0 before:transiton before:duration-200 before:ease-in-out overflow-x-hidden">
+                <li class="relative text-base font-medium before:absolute before:w-full before:h-[1px] before:bottom-0 before:bg-primary dark:before:bg-slate-third before:-left-full hover:before:left-0 before:transiton before:duration-200 before:ease-in-out overflow-x-hidden">
                     <a href="#faq">
                         <span class="span dark:text-slate-third">F</span>aq
                     </a>
                 </li>
-                <li class="relative text-base font-medium before:absolute before:w-full before:h-[1px] before:bottom-0 before:bg-primary before:-left-full hover:before:left-0 before:transiton before:duration-200 before:ease-in-out overflow-x-hidden">
+                <li class="relative text-base font-medium before:absolute before:w-full before:h-[1px] before:bottom-0 before:bg-primary dark:before:bg-slate-third before:-left-full hover:before:left-0 before:transiton before:duration-200 before:ease-in-out overflow-x-hidden">
                     <a href="#contact">
                         <span class="span dark:text-slate-third">C</span>ontact
                     </a>
