687458fd64fec64883e9ee62431e9e59775d2fae
Adnan Erlansyah <adnanerlansyah403@gmail.com>
2023-01-23 16:23:47 +0700

    styling newpassword page

--- resources/views/pages/auth/new-password.blade.php
+++ resources/views/pages/auth/new-password.blade.php
@@ -21,11 +21,19 @@
 </div>
 
 <template x-if="!isLoadingAuth">
-    <div class="container mx-auto flex items-start" >
+    <div class="container mx-auto flex items-start justify-center">
 
-        <div class="col col-12 lg:col-6">
+        <div class="col col-12 md:col-6 lg:col-6 py-6 px-4 border border-gray-third bg-white dark:bg-slate-secondary rounded-primary lg:flex flex-col items-center">
 
-            <div class="mb-8" x-data="{passwordHidden: true}">
+            <div class="mb-8 text-center">
+                <h1 class="text-[32px]">
+                    <span class="span font-medium">New</span>
+                    Password
+                </h1>
+                <p class="mt-2 mb-4 text-[14px] text-gray-primary">Please enter your new password, and the password will be changed </p>
+            </div>
+        
+            <div class="w-full" x-data="{passwordHidden: true}">
                 <label for="password" class="text-md">New Password</label>
                 <div class="group flex items-center gap-4 pl-4 pr-3 py-4 w-full shadow-[0px_0px_4px_rgba(0,0,0,0.25)] bg-white hover:bg-white dark:bg-slate-secondary rounded-primary mt-4 transition duration-200 ease-in-out">
                     <button type="button" @click="
@@ -54,29 +62,23 @@
                     <input type="password" placeholder="Your password..."
                     x-bind:class="status_err.password ? 'input-danger' : ''" class="w-full" x-model="password" x-ref="password">
                 </div>
-                    <template x-if="status_err.password">
-                        <div class="mt-3 flex text-[#b91c1c] items-center gap-2">
-                            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
-                            <span class="span-danger" x-text="status_err.password[0]">Validasi Error</span>
-                        </div>
-                    </template>
+                <template x-if="status_err.password">
+                    <div class="mt-3 flex text-[#b91c1c] items-center gap-2">
+                        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+                        <span class="span-danger" x-text="status_err.password[0]">Validasi Error</span>
+                    </div>
+                </template>
+                <div class="flex items-center justify-center mt-6">
+                    <button type="submit"
+                        class="px-4 py-2 rounded-pill bg-primary dark:bg-slate-secondary text-white text-center hover:text-opacity-90 transition duration-200 ease-in-out" @click="newPassword({{Request::segment(2)}})">
+                        Confirm
+                    </button>
+                </div>
             </div>
 
-            <div class="flex items-center justify-between mb-14">
-                <button type="submit"
-                    class="px-4 py-2 rounded-pill bg-primary dark:bg-slate-secondary text-white text-center hover:text-opacity-90 transition duration-200 ease-in-out" @click="newPassword({{Request::segment(2)}})">
-                    Confirm
-                </button>
-            </div>
 
         </div>
-
-        <div class="border border-gray-third bg-white dark:bg-slate-secondary rounded-primary hidden lg:flex col col-6 h-[400px] items-center justify-center">
-            <figure>
-                <img src="{{ asset('assets/images/accessaccount.svg') }}" class="w-[300px] h-[300px]" alt="">
-            </figure>
-        </div>
-
+        
     </div>
 </template>
 
