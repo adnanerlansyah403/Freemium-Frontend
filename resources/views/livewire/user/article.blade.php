9f974bb015cf272ef08876427995632949e4c0d8
Adnan Erlansyah <adnanerlansyah403@gmail.com>
2023-01-23 08:28:50 +0700

    updated myarticle

--- resources/views/livewire/user/article.blade.php
+++ resources/views/livewire/user/article.blade.php
@@ -58,7 +58,7 @@
                                     </div>
                                     <div class="pb-4 pt-12 lg:py-0 lg:translate-y-5">
                                         <div class="flex items-center justify-between mb-4">
-                                            <span class="text-base md:text-md font-neucha font-bold" x-text="item.title"></span>
+                                            <a x-bind:href="baseUrl + `article/detail/${item.id}`" class="text-base md:text-md font-neucha font-bold" x-text="item.title"></a>
                                             <i class="bg-primary dark:bg-slate-third px-4 py-2 rounded-primary text-white font-bold" x-text="item.type.charAt(0).toUpperCase() + item.type.slice(1)"></i>
                                         </div>
                                         <p class="text-sm text-gray-secondary mt-8 w-full" x-html="item.substring+'...'">
