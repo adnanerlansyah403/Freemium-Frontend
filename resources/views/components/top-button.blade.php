7dd6616e62df20f1387b295c768882ff24221e5f
maulana48 <hidarizero4869@gmail.com>
2023-01-23 10:50:32 +0700

    list&create : top button

--- resources/views/components/top-button.blade.php
+++ resources/views/components/top-button.blade.php
@@ -6,26 +6,33 @@
     }
 </style>
 
-<a id="topButton" href="{{ $url }}" class="fixed -bottom-full right-14 p-3 bg-white dark:bg-slate-secondary hover:bg-primary dark:text-white hover:text-white dark:hover:text-opacity-80 rounded-full shadow-[0px_0px_4px_rgba(0,0,0,0.25)] transition duration-200 ease-in-out">
-    <i data-feather="arrow-up" class="animate-bounce" style="position: relative; bottom: -3px;"></i> 
-</a>
+<button id="btn-back-to-top" type="button"
+    class="fixed bottom-[56px] right-14 p-3 dark:bg-slate-secondary bg-primary dark:text-white hover:text-white dark:hover:text-opacity-80 rounded-full shadow-[0px_0px_4px_rgba(0,0,0,0.25)] transition duration-200 ease-in-out"
+    title="Instructions">
+    <i data-feather="arrow-up" class="animate-bounce bottom-[3px]"></i>
+</button>
 
 <script>
+    // Get the button
+    let mybutton = document.getElementById('btn-back-to-top');
     
-    const topButton = document.getElementById('topButton');
-    window.addEventListener('load', function () {
-        if(window.scrollY > 200) {
-            topButton.classList.add('active');
-        } else {
-            topButton.classList.remove('active');
-        }
-    });
-    window.addEventListener("scroll", function () {
-        if(window.scrollY > 200) {
-            topButton.classList.add("active");
-        } else {
-            topButton.classList.remove("active");
-        }
-    })
-
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () {
+    scrollFunction();
+    };
+    
+    function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = 'block';
+    } else {
+    mybutton.style.display = 'none';
+    }
+    }
+    // When the user clicks on the button, scroll to the top of the document
+    mybutton.addEventListener('click', backToTop);
+    
+    function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    }
 </script>
